660ac99fbe783e233bc757e873b04bc14fc4ffc8
John Tuttle <thejwt@gmail.com>
2017-11-03 19:23:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,8 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     user = User.find_by(username: params[:username])
     #binding.pry
-    session[:user_id] = user.id
     if user.id != nil
-      ""
+      session[:user_id] = user.id
       redirect '/account'
     else
       erb :error
