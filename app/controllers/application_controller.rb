790f1dedf0915c9f449d5404c06918e94aa85f5a
John Tuttle <thejwt@gmail.com>
2017-11-03 19:04:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    
+    @user = User.find_by(username: params[:username]
+
     if @user != nil
       session[:user_id] = @user.id
       "#{@user.account}"
